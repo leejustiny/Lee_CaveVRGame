629a415b395b713d608deb69991d4fd64181e3a2
Justin Y Lee <jylee@scu.edu>
2018-11-02 19:42:04 -0700

    Project Upload

---
+++ Assets/Scripts/RegenItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegenItem : MonoBehaviour { //should be on ground collider
+
+    public GameObject originalItem;
+    public Vector3 spawnLocation;
+    public GameObject instantiatedObject;
+
+
+
+    // Use this for initialization
+    void Start () {
+        spawnLocation = originalItem.transform.position;
+        instantiatedObject.transform.position = spawnLocation;
+    }
+
+    public GameObject OnTriggerEnter(Collider item) //item is pickaxe or flashlight
+    {
+
+        if (item.gameObject.tag == ("Pickaxe") || item.gameObject.tag == ("flashlight"))  //if this item is pickaxe or flashlight
+        {
+            // Regenitem(item, itemComp); //gameobject.name
+            originalItem = item.gameObject;
+
+            instantiatedObject = (GameObject) Instantiate(originalItem);
+            
+            
+            
+            
+        }
+
+        Destroy(originalItem);
+
+        return instantiatedObject;
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+	}
+
+
+}
---
+++ Assets/Scripts/TurnFlashOn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnFlashOn : MonoBehaviour {
+
+    public GameObject flashlight;
+    public GameObject spotlight;
+    //public Component beamlight;
+    
+
+    // Use this for initialization
+    void Start () {
+		flashlight = GameObject.FindWithTag("flashlight");
+        spotlight = GameObject.FindWithTag("spotlight");
+
+
+        spotlight.GetComponent<Light>().enabled = (false);
+    }
+
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void onTriggerEnter(Collider hand)
+    {
+        if (hand.CompareTag("hand"))
+        {
+
+            spotlight.GetComponent<Light>().enabled = (true);
+        }
+
+    }
+}
